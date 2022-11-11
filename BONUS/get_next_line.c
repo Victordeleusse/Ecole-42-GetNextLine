b3b342814dfdca90c8bfa26e02880b9eb04771b1
Your Name <you@example.com>
2022-11-11 16:50:16 +0100

    avec les bonus

---
+++ BONUS/get_next_line.c
@@ -0,0 +1,194 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-leus <vde-leus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 10:41:12 by vde-leus          #+#    #+#             */
+/*   Updated: 2022/11/11 16:38:40 by vde-leus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+t_list	*ft_generate_liste(t_list **begin, int fd)
+{
+	t_list	*liste;
+	t_list	*suivant;
+
+	if (!*begin)
+		return (NULL);
+	liste = *begin;
+	if ((*begin)->data[0] != '\0')
+	{	
+		if (ft_fin_ligne((*begin)->data) == 1)
+			return (liste);
+		suivant = ft_generate_element();
+		(*begin)->next = suivant;
+		*begin = suivant;
+	}
+	while (read(fd, (*begin)->data, BUFFER_SIZE) > 0
+		&& ft_fin_ligne((*begin)->data) == 0)
+	{	
+		suivant = ft_generate_element();
+		(*begin)->next = suivant;
+		(*begin) = suivant;
+	}
+	return (liste);
+}
+
+char	*ft_join(t_list *liste)
+{
+	size_t	len;
+	size_t	i;
+	size_t	j;
+	char	*resultat;
+	t_list	*suivant;
+
+	len = ft_liste_size(liste);
+	resultat = (char *)malloc(sizeof(char) * (len * BUFFER_SIZE + 1));
+	if (!resultat)
+		return (NULL);
+	resultat[len * BUFFER_SIZE] = '\0';
+	j = 0;
+	while (liste->next)
+	{
+		i = 0;
+		while (liste->data[i])
+			resultat[j++] = liste->data[i++];
+		free(liste->data);
+		suivant = liste->next;
+		free(liste);
+		liste = suivant;
+	}
+	resultat[j] = '\0';
+	return (resultat);
+}
+
+void	ft_traitement(char *resultat, t_list **begin)
+{
+	size_t	j;
+	size_t	i;
+
+	j = 0;
+	i = 0;
+	while (resultat[j])
+		j++;
+	while ((*begin)->data[i] && (*begin)->data[i] != '\n')
+	{	
+		resultat[j++] = (*begin)->data[i];
+		(*begin)->data[i++] = '\0';
+	}
+	resultat[j] = (*begin)->data[i];
+	if ((*begin)->data[i] == '\n')
+		(*begin)->data[i++] = '\0';
+	if (resultat[j] == '\n')
+		resultat[++j] = '\0';
+	j = 0;
+	while ((*begin)->data[i])
+		(*begin)->data[j++] = (*begin)->data[i++];
+	(*begin)->data[j] = '\0';
+}
+
+void	ft_clear(t_fdlist **fd_liste, t_fdlist *fd_element)
+{
+	t_fdlist	*start;
+
+	start = *fd_liste;
+	if (start->id_fd == fd_element->id_fd)
+	{	
+		*fd_liste = start->next_fd;
+		free(start->begin->data);
+		free(start->begin);
+		// free(start->next_fd);
+		free(start);
+	}
+	else
+	{
+		while (start->next_fd->id_fd != fd_element->id_fd)
+			start = start->next_fd;
+		start->next_fd = fd_element->next_fd;
+		free(fd_element->begin->data);
+		free(fd_element->begin);
+		// free(fd_element->next_fd);
+		free(fd_element);
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static t_fdlist	*fd_liste;
+	t_fdlist		*fd_element;
+	t_list			*liste;
+	char			*resultat;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!fd_liste)
+		fd_liste = ft_generate_fd_element(fd);
+	fd_element = ft_recup_fd_list(&fd_liste, fd);
+	liste = ft_generate_liste(&fd_element->begin, fd);
+	resultat = ft_join(liste);
+	ft_traitement(resultat, &fd_element->begin);
+	if (resultat[0] == '\0')
+	{
+		ft_clear(&fd_liste, fd_element);
+		free(resultat);
+		return (NULL);
+	}
+	return (resultat);
+}
+
+// int	main(void)
+// {
+// 	char	*resultat;
+// 	int		fd1;
+// 	int		fd2;
+
+// 	fd1 = open("text.txt", O_RDONLY);
+// 	fd2 = open("text2.txt", O_RDONLY);
+// 	//fd = 0;
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT venant de 1 : %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT venant de 1 : %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT venant de 2 : %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd2);
+// 	printf("LE RESULTAT  venant de 2: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+// 	resultat = get_next_line(fd1);
+// 	printf("LE RESULTAT  venant de 1: %s\n", resultat);
+
+// 	return (0);
+// }
--- BONUS/get_next_line.h
+++ BONUS/get_next_line.h
@@ -5,25 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vde-leus <vde-leus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 14:20:09 by vde-leus          #+#    #+#             */
-/*   Updated: 2022/10/10 13:38:34 by vde-leus         ###   ########.fr       */
+/*   Created: 2022/11/11 10:23:41 by vde-leus          #+#    #+#             */
+/*   Updated: 2022/11/11 15:50:40 by vde-leus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 typedef struct s_list
 {
@@ -31,16 +30,22 @@ typedef struct s_list
 	struct s_list	*next;
 }t_list;
 
-void	*ft_memset(void *b, int c, size_t len);
-char	*ft_strjoin(t_list *liste);
-int		ft_endligne(char *str);
-size_t	ft_sizelist(t_list *begin_list);
-t_list	*ft_generate_element(void);
+typedef struct m_list
+{
+	t_list			*begin;
+	int				id_fd;
+	struct m_list	*next_fd;
+}t_fdlist;
 
-t_list	*ft_generate_liste(t_list *buffer, int fd);
-void	ft_generate_buffer(t_list *liste, t_list **buffer);
-char	*ft_traitement_buffer(t_list *buffer);
-void	ft_clean_data(t_list *liste);
-char	*get_next_line(int fd);
+t_list		*ft_generate_element(void);
+int			ft_fin_ligne(char *str);
+size_t		ft_liste_size(t_list *liste);
+t_list		*ft_generate_liste(t_list **begin, int fd);
+char		*ft_join(t_list *liste);
+void		ft_traitement(char *resultat, t_list **begin);
+char		*get_next_line(int fd);
+t_fdlist	*ft_generate_fd_element(int fd);
+t_fdlist	*ft_recup_fd_list(t_fdlist **fd_liste, int fd);
+void		ft_clear(t_fdlist **fd_liste, t_fdlist *fd_element);
 
 #endif
